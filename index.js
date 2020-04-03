9344e739c74a865cd3cebaa3daf7a76cf16c3a08
ganmelLP <54279079+ganmelLP@users.noreply.github.com>
2020-04-03 10:40:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const path = require('path');
+const router = express.Router();
 
-app.get('/', (req, res) => res.send('Hello World!'))
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
