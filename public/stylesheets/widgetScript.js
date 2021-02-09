3fe875477b5046047881fab500696bed6506c7ec
ganmelLP <ganm@liveperson.com>
2021-02-09 10:41:36 +0000

    ff

--- public/stylesheets/widgetScript.js
+++ public/stylesheets/widgetScript.js
@@ -97,8 +97,8 @@ document.onreadystatechange = () => {
                                     showUsedBrands(noUsedDuplicates, usedBrandName);
 
                                     // if (SDEResult.vehicleOfInterest[0].voi.stockType == "NEW") {
-
-                                    //     optionBrand = SDEResult.vehicleOfInterest[0].voi.make;
+                                    console.log(`Printing from inside the API Calls > ${contextFull}, Location: ${contextFull.locationName}`)
+                                    //optionBrand = SDEResult.vehicleOfInterest[0].voi.make;
                                     //     brandName.getElementsByTagName('option')[0].innerHTML = optionBrand;
                                     //     brandName.getElementsByTagName('option')[0].setAttribute("class", "brand selection")
                 
