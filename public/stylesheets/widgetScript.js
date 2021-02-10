53c7bfda77ea99176544fea287fc677e7d9fbd96
ganmelLP <ganm@liveperson.com>
2021-02-10 18:43:37 +0000

    jj

--- public/stylesheets/widgetScript.js
+++ public/stylesheets/widgetScript.js
@@ -177,8 +177,7 @@ document.onreadystatechange = () => {
                                                                 // if (SDEResult.vehicleOfInterest[0].voi.stockType == "NEW") {
                                                                 console.log(`Printing from inside the API Calls > ${JSON.stringify(contextFull)}, Location: ${contextFull.locationName}`)
 
-                                                                document.getElementsByClassName('thead')[0].innerHTML = contextFull.locationName
-                                                                document.getElementsByClassName('thead')[0].value = contextFull.locationName
+                                                                $("#typeahead-autocomplete .thead").typeahead('val',contextFull.locationName);
 
                                                                 //optionBrand = SDEResult.vehicleOfInterest[0].voi.make;
                                                                 //     brandName.getElementsByTagName('option')[0].innerHTML = optionBrand;
