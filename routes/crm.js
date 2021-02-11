f2718c46ab304e0e09ac5886c290fdfabbaf7ad7
ganmelLP <ganm@liveperson.com>
2021-02-11 16:10:41 +0000

    f

--- routes/crm.js
+++ routes/crm.js
@@ -33,7 +33,7 @@ function leadPost(req, res, data) {
     uri: 'https://api-preprod.robinsandday.co.uk/api/v1/lead',
     headers:{
       'accept': 'application/json',
-      'authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImU0NGZmZTI3Yjc2NjhkNzg2NGMzMWMwMWI1NDMwNWY5MjRiNTMzZTY2YWYxZDY0MDRiMTg4NjE5ZWJlMmUxYzE4MDYwZDIxNzZlNmJiZDJjIn0.eyJhdWQiOiIyIiwianRpIjoiZTQ0ZmZlMjdiNzY2OGQ3ODY0YzMxYzAxYjU0MzA1ZjkyNGI1MzNlNjZhZjFkNjQwNGIxODg2MTllYmUyZTFjMTgwNjBkMjE3NmU2YmJkMmMiLCJpYXQiOjE2MTIxODM5MDUsIm5iZiI6MTYxMjE4MzkwNSwiZXhwIjoxNjQzNzE5OTA1LCJzdWIiOiI1Iiwic2NvcGVzIjpbXX0.bllmfo8dt7FO9HNsVGptj5zm4k3Fx_Uk-cR1IVO0TxmoGvBzB7SDHr5PWYoJEU6b_3MSDOTPsMgEgqN3d2URuY6Alfyh8f4K6Q18awnXRK4p7CZrUb9yJK3fj9nWkwYytGyl76UJKN2fKUjd8KVltoHHM6-KzQl0pMvoyoditUbLiYFoNLcQd_tzz5BvrNoy1IdbmiqXaDvinsii00TsgTaq_fckaNVWwkoTp3qDb_YVYs1ZW6XK-mVvmKSD96_8Zu2W4haV2f3qfLR5OrvCfYQeZWnJ5XRW7aIMzPmxnffpgXBTJnYlIXE8UxVpF1P0Bfwtw2VZSgnmd3Cj-rkOyXIBKguU92JumcNjRx2gEaIQtGdxEwlcmZuFGCX2jSnChtNATfqnn6QDx9A-VdokxorPcd6TTK5XreorjMDnto40nbqYeBKdpw7NtMdzA4dWOmmwcWTGuwNNytBHFRb9FFAbOxMxHn3cOFFjdKHICC-gnV3T_o4AL8UpboM1H-PGFkwiO5a9VIG9exLYV6Blg6YfeMYufkLSH1Ye9dwoKOmfMIP7oqO1XAo00XCiaqMst7Yrzl_6eya2cWPkXV_3HHAD3zoLnptwX58RC7Bp7eqzbHD5nvkcWIHiVpCCDf6vOGzXAbVVAKVhSVGVtUBUi-4blyuFe4WwQNXOiubJMjU',
+      'authorization': ,
       'content-type': 'application/json',
     },
     body:{
