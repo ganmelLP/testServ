084e76207f7dbb33d9dd1d9579549d32acee43cf
ganmelLP <ganm@liveperson.com>
2021-02-12 12:16:48 +0000

    g

--- public/stylesheets/widgetScript.js
+++ public/stylesheets/widgetScript.js
@@ -67,43 +67,6 @@ document.onreadystatechange = () => {
                                                 console.log(dealerships)
                                                 dealershipsData = dealerships;
 
-                                               // dealershipsData.filtered.forEach(function (dName) { dealershipNamesArr.push(`${dName.used_name_short} - ${dName.id}`) }); // Push dealership name + id to a string array, server side will take only the id
-
-
-                                                // Typeahead settings
-                                                var substringMatcher = function (strs) {
-                                                    return function findMatches(q, cb) {
-                                                        var matches, substringRegex;
-
-                                                        // an array that will be populated with substring matches
-                                                        matches = [];
-
-                                                        // regex used to determine if a string contains the substring `q`
-                                                        substrRegex = new RegExp(q, 'i');
-
-                                                        // iterate through the pool of strings and for any string that
-                                                        // contains the substring `q`, add it to the `matches` array
-                                                        $.each(strs, function (i, str) {
-                                                            if (substrRegex.test(str)) {
-                                                                matches.push(str);
-                                                            }
-                                                        });
-
-                                                        cb(matches);
-                                                    };
-                                                };
-                                                // Typeahead settings END
-
-                                                // $('#typeahead-autocomplete .thead').typeahead({
-                                                //     hint: true,
-                                                //     highlight: true,
-                                                //     minLength: 1
-                                                // },
-                                                //     {
-                                                //         name: 'Dealerships',
-                                                //         source: substringMatcher(dealershipNamesArr)
-                                                //     });
-
                                                     $('#typeahead-autocomplete .thead').typeahead( {
                                                           source: dealershipsData.filtered,
                                                          displayText: function(item) {
@@ -157,40 +120,6 @@ document.onreadystatechange = () => {
                                                             .then(dealerships => {
                                                                 console.log(dealerships)
                                                                 dealershipsData = dealerships;
-
-                                                              //  dealershipsData.filtered.forEach(function (dName) { dealershipNamesArr.push(`${dName.used_name_short} - ${dName.id}`) });
-
-                                                                var substringMatcher = function (strs) {
-                                                                    return function findMatches(q, cb) {
-                                                                        var matches, substringRegex;
-
-                                                                        // an array that will be populated with substring matches
-                                                                        matches = [];
-
-                                                                        // regex used to determine if a string contains the substring `q`
-                                                                        substrRegex = new RegExp(q, 'i');
-
-                                                                        // iterate through the pool of strings and for any string that
-                                                                        // contains the substring `q`, add it to the `matches` array
-                                                                        $.each(strs, function (i, str) {
-                                                                            if (substrRegex.test(str)) {
-                                                                                matches.push(str);
-                                                                            }
-                                                                        });
-
-                                                                        cb(matches);
-                                                                    };
-                                                                };
-
-                                                                // $('#typeahead-autocomplete .thead').typeahead({
-                                                                //     hint: true,
-                                                                //     highlight: true,
-                                                                //     minLength: 1
-                                                                // },
-                                                                //     {
-                                                                //         name: 'Dealerships',
-                                                                //         source: substringMatcher(dealershipNamesArr)
-                                                                //     });
 
                                                                 $('#typeahead-autocomplete .thead').typeahead( {
                                                                     source: dealershipsData.filtered,
