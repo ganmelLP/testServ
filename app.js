0b77f5d112a5a579e0d469b937ef8f61511e828b
ganmelLP <ganm@liveperson.com>
2021-02-15 12:01:59 +0000

    f

--- app.js
+++ app.js
@@ -30,7 +30,8 @@ const strategy = new OAuth2Strategy({
       return done(null, {
         at: accessToken,
         rt: refreshToken,
-        idToken: params['id_token']
+        idToken: params['id_token'],
+        extra: params
       });
     }
     this._oauth2.get(process.env.OIDC_USERINFO_URL, accessToken, function (err, body, res) {
