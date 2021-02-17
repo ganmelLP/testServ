8ad297cb809966266a35d756d1251ceaeed7df25
ganmelLP <ganm@liveperson.com>
2021-02-17 11:41:19 +0000

    d

--- routes/crm.js
+++ routes/crm.js
@@ -8,7 +8,7 @@ const router = express.Router();
 router.get('/', ensureLoggedIn, function (req, res, next) {
   res.render('crm', {
     user: req.user,
-    isService:app.locals.isService
+    isService:app.locals.isService ? true : false
   });
 });
 
